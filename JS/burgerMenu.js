6f7cbea187f9f25afbf172aed744cb802f73fd2f
daniel-fomkin <danielfomkin87@gmail.com>
2026-09-15 14:32:47 +0300

    Add burger menu and logo

---
+++ JS/burgerMenu.js
@@ -0,0 +1,13 @@
+// HTML elements
+const menuButton = document.querySelector("#menu-btn");
+const closeButton = document.querySelector("#close-btn");
+const menu = document.querySelector("#menu");
+const burger = menu.querySelector(".burger-menu");
+
+console.log(menuButton)
+
+menuButton.addEventListener("click", () => {
+    menu.classList.toggle("hidden");
+    burger.style.animation = "openBurger 1s"
+});
+closeButton.addEventListener("click", () => menu.classList.toggle("hidden"));
